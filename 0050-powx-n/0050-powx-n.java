1fbb735f59a42500dd9d08448ff4786d8b3d4d4b
Harish Buddha <harishh.buddha@gmail.com>
2026-08-09 01:25:10 +0530

    Time: 0 ms (100%), Space: 47.8 MB (65.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+
+        long power= n;
+        double ans=1;
+        if(power<0){
+            x=1/x;
+            power=-power;
+        }
+
+        while(power>0){
+            if(power%2==1){
+                ans= ans*x;
+            }
+            x=x*x;
+            power= power/2;
+        }
+        return ans;
     }
 }
